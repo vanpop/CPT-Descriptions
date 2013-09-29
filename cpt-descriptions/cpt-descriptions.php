e42b64c261a246817349c5301b530ce69ab7e7ef
Evan Stein <estein.work@gmail.com>
2013-09-29 15:25:07 -0400

    new filterable post_types function and cleaner get_post_types() usage

--- cpt-descriptions/cpt-descriptions.php
+++ cpt-descriptions/cpt-descriptions.php
@@ -7,8 +7,6 @@ Description: Adds a place to enter a description for your custom post types whic
 Author: Evan Stein
 Author URI: http://vanpop.com/
 License: GPL v3
-Text Domain: cptdescriptions
-Domain Path: /localization/
 
 CPT Descriptions
 Copyright (C) 2013, Evan Stein - admin@vanpop.com
@@ -27,92 +25,44 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-load_plugin_textdomain(
-    'cptdescriptions',
-    false,
-    dirname( plugin_basename( __FILE__ ) ) . '/localization/'
-);
-
 add_action( 'admin_init', 'post_type_desc_register_settings' );
 add_action( 'admin_menu' , 'post_type_desc_enable_pages' );
 
-function post_type_desc_register_settings() {
+function cptd_get_post_types() {
   $args = array(
     'public'   => true,
     '_builtin' => false
   );
-  $output = 'names'; // names or objects, note names is the default
-  $operator = 'and'; // 'and' or 'or'
-  $post_types = get_post_types( $args, $output, $operator );
+  $post_types = apply_filters( 'cptd_post_types', get_post_types( $args ) );
+
+  return $post_types;
+}
+
+function post_type_desc_register_settings() {
+  $post_types = cptd_get_post_types();
 
   foreach ( $post_types as $post_type ) {
-    // Register settings and call sanitation functions
-    register_setting( 'post_type_desc' . $post_type, 'post_type_desc_' . $post_type, 'post_type_desc_validate_options' );
+    if( post_type_exists( $post_type ) ) {
+      // Register settings and call sanitation functions
+      register_setting( 'post_type_desc' . $post_type, 'post_type_desc_' . $post_type, 'post_type_desc_validate_options' );
+    }
   }
 }
 
 function post_type_desc_enable_pages() {
-  $args = array(
-    'public'   => true,
-    '_builtin' => false
-  );
-  $output = 'names'; // names or objects, note names is the default
-  $operator = 'and'; // 'and' or 'or'
-  $post_types = get_post_types( $args, $output, $operator );
+  $post_types = cptd_get_post_types();
 
   foreach ( $post_types as $post_type ) {
-    $args = array(
-      'name' => $post_type,
-    );
-    $output = 'objects'; // names or objects, note names is the default
-    $operator = 'and'; // 'and' or 'or'
-    $post_type_info = get_post_types( $args, $output, $operator );
-
-    $settings_page_title = sprintf( __( 'Description of the %1$s Custom Post Type', 'cptdescriptions' ), $post_type_info[$post_type]->labels->name );
-    $settings_page_title = apply_filters( 'cptd_admin_title', $settings_page_title, $post_type_info[$post_type] );
-
-    $settings_page_menu_label = __( 'Description', 'cptdescriptions' );
-    $settings_page_menu_label = apply_filters( 'cptd_menu_label', $settings_page_menu_label, $post_type_info[$post_type] );
-
-    add_submenu_page(
-      'edit.php?post_type=' . $post_type,
-      $settings_page_title,
-      $settings_page_menu_label,
-      'edit_posts',
-      $post_type . '-description',
-      'post_type_desc_page'
-    );
-  }
-}
-
-// thank you! blog.rutwick.com/add-items-anywhere-to-the-wp-3-3-admin-bar
-function cptd_admin_bar_links( $admin_bar ) {
-  if( !is_admin() && is_post_type_archive() ) {
-    global $post_type;
-    $args = array(
-      'id'    => 'wp-admin-bar-edit',
-      'title' => __( 'Edit Description', 'cptdescriptions' ),
-      'href'  => admin_url( 'edit.php?post_type=' . $post_type . '&page=' . $post_type . '-description' )
-    );
-    $admin_bar->add_menu( $args );
-  }
-  if( is_admin() ) {
-    $screen = get_current_screen();
-    $post_type = $screen->post_type;
-    $description_page = $post_type . '_page_' . $post_type . '-description';
-    if( $screen->base == $description_page ) {
-      $post_type_object = get_post_type_object( $post_type );
+    if( post_type_exists( $post_type ) ) {
       $args = array(
-        'id'    => 'wp-admin-bar-edit',
-        'title' => 'View ' . $post_type_object->labels->name . ' Archive',
-        'href'  => get_post_type_archive_link( $post_type )
+        'name' => $post_type,
       );
-      $admin_bar->add_menu( $args );
+      $post_type_info = get_post_types( $args, 'objects' );
+
+      add_submenu_page( 'edit.php?post_type=' . $post_type, $post_type_info[$post_type]->labels->name . ' Custom Post Type Description', 'Description', 'edit_posts', urlencode( $post_type_info[$post_type]->name ) . '-description', 'post_type_desc_page' );
     }
   }
-
 }
-add_action('admin_bar_menu', 'cptd_admin_bar_links',  100);
 
 function post_type_desc_page() {
   global $post_type_desc;
@@ -121,20 +71,13 @@ function post_type_desc_page() {
   $args = array(
     'name' => $post_type,
   );
-  $output = 'objects'; // names or objects, note names is the default
-  $operator = 'and'; // 'and' or 'or'
-  $post_type_info = get_post_types( $args, $output, $operator );
+  $post_type_info = get_post_types( $args, 'objects' );
 
   if ( ! isset( $_REQUEST['settings-updated'] ) )
     $_REQUEST['settings-updated'] = false; // This checks whether the form has just been submitted. ?>
 
 <div class="wrap">
-  <?php  screen_icon();
-
-  $settings_page_title = sprintf( __( 'Description of the %1$s Custom Post Type', 'cptdescriptions' ), $post_type_info[$post_type]->labels->name );
-  $settings_page_title = apply_filters( 'cptd_admin_title', $settings_page_title, $post_type_info[$post_type] );
-
-  echo "<h2>" . $settings_page_title . "</h2>"; ?>
+  <?php  screen_icon(); echo "<h2>" . __( 'Description of the ' ) . $post_type_info[$post_type]->labels->name . " Custom Post Type</h2>"; ?>
 
   <?php if ( false !== $_REQUEST['settings-updated'] ) : ?>
     <div class="updated fade"><p><strong><?php _e( 'Options saved' ); ?></strong></p></div>
@@ -153,7 +96,7 @@ function post_type_desc_page() {
       wp_editor( stripslashes( $settings['description'] ), 'description', $editor_settings );
       ?>
 
-        <p class="submit"><input type="submit" class="button-primary" value="<?php _e( 'Save Description', 'cptdescriptions' ); ?>" /></p>
+        <p class="submit"><input type="submit" class="button-primary" value="Save Description" /></p>
     </form>
 </div>
 <?php
@@ -166,12 +109,15 @@ function post_type_desc_validate_options( $input ) {
 }
 
 function the_post_type_description( $post_type = '' ) {
-  echo get_post_type_description( $post_type );
+  if ( '' == $post_type )
+    global $post_type;
+  $post_type_x = get_option( 'post_type_desc_' . $post_type );
+  echo wpautop( $post_type_x['description'] );
 }
 
 function get_post_type_description( $post_type = '' ) {
   if ( '' == $post_type )
     global $post_type;
   $post_type_x = get_option( 'post_type_desc_' . $post_type );
-  echo apply_filters( 'the_content', $post_type_x['description'] );
+  return wpautop( $post_type_x['description'] );
 }
